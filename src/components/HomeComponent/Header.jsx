09db339ab009e509c71be0860273aa500eef1c7f
kishanraj04 <kishuraj1111@gmail.com>
2025-04-02 21:21:34 +0530

    implementing dynamic functionality

--- src/components/HomeComponent/Header.jsx
+++ src/components/HomeComponent/Header.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import { Bookmark, Search } from "lucide-react";
 import "../style/header.css";
+import { apiCalling } from "../../api/apiCalling";
+import { useDispatch } from "react-redux";
+import { setAllGames } from "../../store/gameData";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const dispatch = useDispatch()
+  const handleSearch = async(e)=>{
+    const option = {method:"GET",url:`https://api.rawg.io/api/games?key=6d633194af394581991e28eb88ce8c1d&search=${searchTerm}`}
+    const resp = await apiCalling(option)
+    dispatch(setAllGames({data:resp?.data?.results}))
+  }
 
   return (
     <header className="header">
       <div className="logo">GameZone</div>
 
       <div className="search-container">
+      
         <input
           type="text"
           placeholder="Search games..."
@@ -17,7 +27,7 @@ const Header = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           className="search-input"
         />
-        <Search className="search-icon" size={18} />
+        <Search className="search-icon" size={18} onClick={handleSearch}/>
       </div>
 
       <div className="library-section">
